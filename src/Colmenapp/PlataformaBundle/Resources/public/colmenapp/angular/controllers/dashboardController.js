526ddb1eaff05bb15b44a41d68d733dcabe9efe1
oscar <canrodriguez@gmail.com>
2016-11-30 00:14:14 -0300

    crear controlador angular para colmena

--- src/Colmenapp/PlataformaBundle/Resources/public/colmenapp/angular/controllers/dashboardController.js
+++ src/Colmenapp/PlataformaBundle/Resources/public/colmenapp/angular/controllers/dashboardController.js
@@ -5,12 +5,15 @@
 
     var dashboardController = function($scope, $http) {
 
-        $scope.labels = ["2012/2013","2013/2014", "2014/2015","2015/2016"];
-        $scope.data   = [1080, 921, 1366, 1527];
+        //grafico productividad
+        $scope.labels = ["","12/13","13/14", "14/15","15/16"];
+        $scope.data   = [0,1080, 921, 1366, 1527];
 
-        $scope.onClick = function (points, evt) {
-            console.log(points, evt);
-        };
+
+        //grafico alta/baja colmenas
+        $scope.labelsColmenas = ['',1,2,3,4,5,6,7,8,9,10,11,12];
+        $scope.dataColmenas   = ['',120, 150, 155, 155,156, 158, 160, 150,120, 123, 127, 130];
+
 
 
         $scope.labelsEstado = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
