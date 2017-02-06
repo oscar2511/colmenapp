4cd390f0ae1dd5e7409728c731723d01d7de9cf6
Oscar Rodriguez <oscar.rodriguez@leadsius.com>
2017-02-06 13:25:43 -0300

    create subscriber

---
+++ src/Colmenapp/PlataformaBundle/Lib/Subscribers/TestSubscriber.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace namespace Colmenapp\PlataformaBundle\Lib\Subscribers;
+
+
+Class TestSubscriber implemement EventSubscriberInterface
+{
+    protected $logger;
+
+    public function __construct($logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array (
+            KernelBundleEvents::MI_TEST_SUBSCRIBER => 'on' //todo: acá quede
+        )
+    }
+
+
+}
