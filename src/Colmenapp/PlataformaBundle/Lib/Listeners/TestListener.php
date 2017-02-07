43975e07f56072975f994556474f432f69037eea
ubibaldo <canrodriguez@gmail.com>
2017-02-06 22:19:01 -0300

    .

---
+++ src/Colmenapp/PlataformaBundle/Lib/Listeners/TestListener.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Colmenapp\PlataformaBundle\Lib\Listeners;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\DependencyInjection\Container;
+
+class TestListener
+{
+    /** @var  Container */
+    protected $container;
+
+    /**
+     * Construct
+     *
+     * @param Container $container
+     */
+    function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     *
+     * Event listener
+     *
+     * @param GetResponseEvent $event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        var_dump("se ejecuto el listener"); die;
+    }
+
+    /**
+     * @return Container
+     */
+    public function getContainer()
+    {
+        return $this->container;
+    }
+
+    /**
+     * @param Container $container
+     */
+    public function setContainer($container)
+    {
+        $this->container = $container;
+    }
+
+
+}
