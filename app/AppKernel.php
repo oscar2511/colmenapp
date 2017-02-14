ab3aa5ba8363b90769285fcccaeffa8310a04d4f
Oscar Rodriguez <oscar.rodriguez@leadsius.com>
2017-02-14 15:53:17 -0300

    install fos user

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Colmenapp\PlataformaBundle\ColmenappPlataformaBundle(),
             new Avanzu\AdminThemeBundle\AvanzuAdminThemeBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
---
+++ src/Colmenapp/PlataformaBundle/Entity/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Colmenapp\PlataformaBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="Colmenapp\PlataformaBundle\Repository\User")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+}
