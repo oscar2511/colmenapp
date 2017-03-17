47d95d70decc0e27e40463046e37386d9860f834
Oscar Rodriguez <oscar.rodriguez@leadsius.com>
2017-03-17 13:06:41 -0300

    integrar angular2

---
+++ src/Colmenapp/PlataformaBundle/Resources/public/ts/app.component.ts
@@ -0,0 +1,9 @@
+// src/AppBundle/Resources/public/ts/main.ts
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'symfony-loves-angular',
+    template: 'Yes, it does!'
+})
+
+export class AppComponent { }
---
+++ src/Colmenapp/PlataformaBundle/Resources/public/ts/main.ts
@@ -0,0 +1,6 @@
+// src/AppBundle/Resources/public/ts/main.ts
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
