5e07a9f95aabf4b702e6a5db8934533250802ca3
Oscar Rodriguez <oscar.rodriguez@leadsius.com>
2017-03-17 14:00:17 -0300

    .

--- src/Colmenapp/PlataformaBundle/Resources/public/ts/main.js
+++ src/Colmenapp/PlataformaBundle/Resources/public/ts/main.js
@@ -3,6 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // src/AppBundle/Resources/public/ts/main.ts
 var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
 var app_module_1 = require("./app.module");
+var test_module_1 = require("./test.module");
 var platform = platform_browser_dynamic_1.platformBrowserDynamic();
 platform.bootstrapModule(app_module_1.AppModule);
+platform.bootstrapModule(test_module_1.TestModule);
 //# sourceMappingURL=main.js.map
--- src/Colmenapp/PlataformaBundle/Resources/public/ts/main.ts
+++ src/Colmenapp/PlataformaBundle/Resources/public/ts/main.ts
@@ -1,6 +1,8 @@
 // src/AppBundle/Resources/public/ts/main.ts
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
+import { TestModule } from './test.module';
 
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
+platform.bootstrapModule(TestModule);
