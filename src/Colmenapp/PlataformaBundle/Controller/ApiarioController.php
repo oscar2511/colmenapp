47e9a996c5eaacec2672e5150d64ed7423d1281d
Oscar Rodriguez <oscar.rodriguez@leadsius.com>
2017-03-24 13:13:13 -0300

    crear apiario controller

--- src/Colmenapp/PlataformaBundle/Controller/ApiarioController.php
+++ src/Colmenapp/PlataformaBundle/Controller/ApiarioController.php
@@ -3,6 +3,9 @@
 namespace Colmenapp\PlataformaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Colmenapp\PlataformaBundle\Entity\Apiario;
+use Symfony\Component\Validator\Constraints\Date;
 
 class ApiarioController extends Controller
 {
@@ -19,6 +22,28 @@ class ApiarioController extends Controller
         return $this->render('ColmenappPlataformaBundle:Apiario:detalle.html.twig', array(
             'clima' => $climaActual
         ));
+    }
+
+    public function crearAction(Request $request)
+    {
+        $info  = $request->getContent();
+        $data  = json_decode($info,true);
+        $em = $this->getDoctrine()->getManager();
+
+        $apiario = new Apiario();
+
+        $apiario->setNombre($data['nombre']);
+        $apiario->setDireccion($data['direccion']);
+        $apiario->setObservacion($data['observacion']);
+        $apiario->setCreated(new \DateTime("now") );
+
+        $em->persist($apiario);
+        $em->flush();
+
+
+
+
+        var_dump($data); die;
     }
 
 }
--- src/Colmenapp/PlataformaBundle/Resources/public/colmenapp/angular/controllers/apiarioController.js
+++ src/Colmenapp/PlataformaBundle/Resources/public/colmenapp/angular/controllers/apiarioController.js
@@ -5,19 +5,17 @@
 
     var apiarioController = function($scope, $http) {
 
-        //grafico productividad
-        $scope.labels = ["","12/13","13/14", "14/15","15/16"];
-        $scope.data   = [0,1080, 921, 1366, 1527];
 
+      $scope.guardarApiario = function(apiarioForm) {
+        //todo: validar
+        var dataApiario = {};
+        dataApiario.nombre      = apiarioForm.nombre;
+        dataApiario.direccion   = apiarioForm.direccion;
+        dataApiario.observacion = apiarioForm.observacion;
 
-        //grafico alta/baja colmenas
-        $scope.labelsColmenas = ['',1,2,3,4,5,6,7,8,9,10,11,12];
-        $scope.dataColmenas   = ['',120, 150, 155, 155,156, 158, 160, 150,120, 123, 127, 130];
-
-
-
-        $scope.labelsEstado = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-        $scope.dataEstado = [300, 500, 100];
+        $http.post(Routing.generate('apiario_crear'), dataApiario);
+        console.log(dataApiario);
+      };
 
     };
 
