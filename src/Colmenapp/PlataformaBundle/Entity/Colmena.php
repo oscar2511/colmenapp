0b9e10f8588f3817e48f6fc8eb5d24b99c920c7d
Oscar Rodriguez <oscar.rodriguez@leadsius.com>
2017-03-30 17:39:48 -0300

    crear entidad colmena

---
+++ src/Colmenapp/PlataformaBundle/Entity/Colmena.php
@@ -0,0 +1,327 @@
+<?php
+
+namespace Colmenapp\PlataformaBundle\Entity;
+
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+
+/**
+ * Leadsius\PlatformBundle\Entity\PlAccount
+ *
+ * @ORM\Table(name="colmena")
+ * @ORM\Entity(repositoryClass="Colmenapp\PlataformaBundle\Repository\ColmenaRepository")
+ */
+class Colmena
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="identificador", type="string", length=255)
+     */
+    protected $identificador;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Apiario")
+     * @assert\NotBlank()
+     */
+    protected $apiario;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="tipo", type="string", length=255, nullable=true)
+     */
+    protected $tipo;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="rejilla_excluidora", type="boolean", nullable=true)
+     */
+    protected $rejillaExcluidora;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="camara_cria", type="integer", nullable=true)
+     */
+    protected $camaraCria;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="en_observacion", type="boolean", nullable=true)
+     */
+    protected $enObservacion;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="ultima_visita", type="datetime", nullable=true)
+     */
+    protected $ultimaVisita;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="estado", type="text", nullable=true)
+     */
+    protected $estado;
+
+    /**
+     * @var \DateTime    $created
+     *
+     * @ORM\Column(name="created", type="datetime", nullable=false)
+     * @Gedmo\Timestampable(on="create")
+     */
+    protected $created;
+
+    /**
+     * @var \DateTime    $updated
+     *
+     * @ORM\Column(name="updated", type="datetime", nullable=true)
+     * @Gedmo\Timestampable(on="update")
+     */
+    protected $updated;
+
+    /**
+     * @var \Datetime $deleted
+     *
+     * @ORM\Column(name="deleted", type="datetime", nullable=true)
+     */
+    protected $deleted;
+
+    public function __toString()
+    {
+        return $this->identificador;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIdentificador()
+    {
+        return $this->identificador;
+    }
+
+    /**
+     * @param string $identificador
+     */
+    public function setIdentificador($identificador)
+    {
+        $this->identificador = $identificador;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiario()
+    {
+        return $this->apiario;
+    }
+
+    /**
+     * @param string $apiario
+     */
+    public function setApiario($apiario)
+    {
+        $this->apiario = $apiario;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+
+    /**
+     * @param string $tipo
+     */
+    public function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isRejillaExcluidora()
+    {
+        return $this->rejillaExcluidora;
+    }
+
+    /**
+     * @param boolean $rejillaExcluidora
+     */
+    public function setRejillaExcluidora($rejillaExcluidora)
+    {
+        $this->rejillaExcluidora = $rejillaExcluidora;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCamaraCria()
+    {
+        return $this->camaraCria;
+    }
+
+    /**
+     * @param int $camaraCria
+     */
+    public function setCamaraCria($camaraCria)
+    {
+        $this->camaraCria = $camaraCria;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isEnObservacion()
+    {
+        return $this->enObservacion;
+    }
+
+    /**
+     * @param boolean $enObservacion
+     */
+    public function setEnObservacion($enObservacion)
+    {
+        $this->enObservacion = $enObservacion;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isUltimaVisita()
+    {
+        return $this->ultimaVisita;
+    }
+
+    /**
+     * @param boolean $ultimaVisita
+     */
+    public function setUltimaVisita($ultimaVisita)
+    {
+        $this->ultimaVisita = $ultimaVisita;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    /**
+     * @param string $estado
+     */
+    public function setEstado($estado)
+    {
+        $this->estado = $estado;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * @param \DateTime $created
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+
+    /**
+     * @param \DateTime $updated
+     */
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+    }
+
+    /**
+     * @return \Datetime
+     */
+    public function getDeleted()
+    {
+        return $this->deleted;
+    }
+
+    /**
+     * @param \Datetime $deleted
+     */
+    public function setDeleted($deleted)
+    {
+        $this->deleted = $deleted;
+    }
+
+
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        return array (
+            'id'                => $this->id,
+            'identificador'     => $this->identificador,
+            'apiario'           => $this->apiario,
+            'tipo'              => $this->tipo,
+            'rejillaExcluidora' => $this->rejillaExcluidora,
+            'camaraCria'        => $this->camaraCria,
+            'enObservacion'     => $this->enObservacion,
+            'ultimaVisita'      => $this->ultimaVisita,
+            'estado'            => $this->estado,
+            'created'           => $this->created
+        );
+    }
+
+
+}
--- src/Colmenapp/PlataformaBundle/Resources/public/colmenapp/angular/controllers/colmenaController.js
+++ src/Colmenapp/PlataformaBundle/Resources/public/colmenapp/angular/controllers/colmenaController.js
@@ -1,21 +1,183 @@
 
 (function() {
 
-    'use strict';
+  'use strict';
 
-    var colmenaController = function($scope, $http) {
+  var colmenaController = function($scope, $http, i18nService, toaster, notificacionService, loader) {
 
-        //grafico numero de abejas en colonia
-        $scope.labels = ["", "Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
-        $scope.data   = [0, 200, 180, 170, 190, 210, 250, 270, 270, 210, 210, 240, 260];1
+    i18nService.setCurrentLang('es'); //idioma de tabla
 
+    $scope.colmena      = {};
+    $scope.mostrarModal = false;
+
+
+    /**
+     * Crear una colmena
+     * @param colmenaForm
+     */
+    $scope.guardarColmena = function(colmenaForm) {
+      loader.start();
+      var dataApiario = {};
+      dataColmena.nombre      = colmenaForm.nombre;
+      dataColmena.direccion   = colmenaForm.direccion;
+      dataColmena.observacion = colmenaForm.observacion;
+
+      $http.post(Routing.generate('apiario_crear'), dataApiario)
+        .then(function(response) {
+          if(response.data.data = 200 && response.data.data) {
+            notificacionService.mostrarNotificacion('success', "Apiario Guardado!", "");
+          }
+          getApiarios();
+          setTable();
+        })
+        .finally(function() {
+          loader.complete();
+        });
     };
 
-    angular.module('colmenapp.Controllers')
-        .controller('colmenaController', [
-            '$scope',
-            '$http',
-            colmenaController
-        ]);
+    /**
+     * Obtener todos los apiarios y crear tabla
+     */
+    function getColmenas(idApiario) {
+      loader.start();
+      $http.get(Routing.generate('colmenas'))
+        .then(function(response) {
+          if(response.data.status == 200) {
+            $scope.gridOptionsApiario.data = response.data.data;
+          }
+        }).finally(function() {
+          loader.complete();
+        });
+    }
+
+    function setTable() {
+      $scope.gridOptionsApiario = {
+        paginationPageSizes: [10, 25, 50],
+        paginationPageSize: 5,
+        enableSorting: true,
+        enableFiltering: true,
+        rowHeight: 35,
+        enableHorizontalScrollbar: 0,
+        enableVerticalScrollbar:0,
+        enableColumnResizing: true,
+        plugins: [new ngGridFlexibleHeightPlugin()],
+        columnDefs: [
+          {
+            field: 'id', displayName: 'Identificador',
+            enableColumnResizing: true,
+            headTemplate: '<div align="center">{{row.entity.id}}</div>'
+          },
+          {
+            field: 'identificador',
+            enableColumnResizing: true
+          },
+          {field: 'apiario'},
+          {
+            field: 'tipo',
+            enableFiltering: false,
+            enableHiding: false
+          },
+          {
+            field: 'Rejilla Excluidora',
+            cellTemplate: '<div align="center" class="ngCellText"><a ng-href="detalle/{{row.entity.id}}"><i class="fa fa-fw fa-eye"></i></a></div>',
+            enableSorting: false,
+            enableFiltering: false,
+            enableHiding: false
+          },
+          {
+            field: 'Camara Cria',
+            cellTemplate: '<div align="center" data-toggle="modal" data-target=".modal-editar-apiario" class="ngCellText"><a ng-href="#" ng-click="grid.appScope.mostrarEditar(row.entity)"><i class="fa fa-fw fa-edit"></i></a></div>',
+            enableSorting: false,
+            enableFiltering: false,
+            enableHiding: false
+          },
+          {
+            field: 'En Observacion',
+            cellTemplate: '<div align="center" data-toggle="modal" data-target=".modal-editar-apiario" class="ngCellText"><a ng-href="#" ng-click="grid.appScope.mostrarEditar(row.entity)"><i class="fa fa-fw fa-edit"></i></a></div>',
+            enableSorting: false,
+            enableFiltering: false,
+            enableHiding: false
+          },
+          {
+            field: 'Ultima Visita',
+            cellTemplate: '<div align="center" data-toggle="modal" data-target=".modal-editar-apiario" class="ngCellText"><a ng-href="#" ng-click="grid.appScope.mostrarEditar(row.entity)"><i class="fa fa-fw fa-edit"></i></a></div>',
+            enableSorting: false,
+            enableFiltering: false,
+            enableHiding: false
+          },
+          {
+            field: 'Estado',
+            cellTemplate: '<div align="center" data-toggle="modal" data-target=".modal-editar-apiario" class="ngCellText"><a ng-href="#" ng-click="grid.appScope.mostrarEditar(row.entity)"><i class="fa fa-fw fa-edit"></i></a></div>',
+            enableSorting: false,
+            enableFiltering: false,
+            enableHiding: false
+          },
+          {
+            field: 'Ver',
+            cellTemplate: '<div align="center" data-toggle="modal" data-target=".modal-editar-apiario" class="ngCellText"><a ng-href="#" ng-click="grid.appScope.mostrarEditar(row.entity)"><i class="fa fa-fw fa-edit"></i></a></div>',
+            enableSorting: false,
+            enableFiltering: false,
+            enableHiding: false
+          },
+          {
+            field: 'Editar',
+            cellTemplate: '<div align="center" data-toggle="modal" data-target=".modal-editar-apiario" class="ngCellText"><a ng-href="#" ng-click="grid.appScope.mostrarEditar(row.entity)"><i class="fa fa-fw fa-edit"></i></a></div>',
+            enableSorting: false,
+            enableFiltering: false,
+            enableHiding: false
+          }
+        ],
+        onRegisterApi: function (gridApi) {
+          $scope.grid1Api = gridApi;
+        }
+      }
+    }
+
+    /**
+     * Mostrar modal editar apiario
+     * @param apiario
+     */
+    $scope.mostrarEditar = function(apiario) {
+      console.log(apiario);
+      $scope.editForm = {
+        id          : apiario.id,
+        nombre      : apiario.nombre,
+        direccion   : apiario.direccion,
+        observacion : apiario.observacion
+      };
+    };
+
+    /**
+     * Editar un apiario
+     * @param apiario
+     */
+    $scope.editar = function(apiario) {
+      $http.post(Routing.generate('apiario_editar'), apiario)
+        .then(function(response) {
+          if(response.data.data = 200 && response.data.data) {
+            notificacionService.mostrarNotificacion('success', "Apiario Editado!", "");
+          }
+          getApiarios();
+          setTable();
+        }).finally(function() {
+          loader.complete();
+        })
+    };
+
+    setTable();
+    getColmenas();
+
+  };
+
+  angular.module('colmenapp.Controllers')
+    .controller('colmenaController', [
+      '$scope',
+      '$http',
+      'i18nService',
+      'toaster',
+      'notificacionService',
+      'cfpLoadingBar',
+      colmenaController
+    ]);
 
 })();
